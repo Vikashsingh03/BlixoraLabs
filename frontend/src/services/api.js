f5ef01b06969a3aed61162b78a94661f17865f3b
Vikash Kumar Singh <vikashsinghbxr8538@gmail.com>
2025-08-10 12:47:14 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
 
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://blixoralabs-backend.onrender.com";
 export default BASE_URL;
