d6bb63d5d3cc906ee8e4f0b79bc4cf02933f05b8
Vikash Kumar Singh <vikashsinghbxr8538@gmail.com>
2025-08-10 13:01:35 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:8000/api/auth/login", {
+      const res = await axios.post("https://blixoralabs-backend.onrender.com/api/auth/login", {
         email,
         password,
       });
