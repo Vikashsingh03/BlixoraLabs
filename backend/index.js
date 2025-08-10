b305c6cf0ba5ccb76cff6cd0f8be425912a6c9cd
Vikash Kumar Singh <vikashsinghbxr8538@gmail.com>
2025-08-10 14:10:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin:["https://blixoralabs-frontend.onrender.com","http://localhost:5173"]
+  origin:["https://blixoralabs-frontend.onrender.com","http://localhost:5173"],
   credentials: true, 
 }));
 
